0954332287d852fcee549d135b5a81fbb77d879e
7ayush <51189189+7ayush@users.noreply.github.com>
2020-08-21 14:46:21 +0530

    This is analog clock project html css

---
+++ index.js
@@ -0,0 +1,14 @@
+    const hour=document.querySelector("#hour");
+    const min=document.querySelector("#min");
+    const sec=document.querySelector("#sec");
+    console.log("Server is running");
+    setInterval(function(){
+    
+    var d=new Date();
+    var hr=d.getHours()*30+d.getMinutes()/2;
+    var mn=d.getMinutes()*6;
+    var sc=d.getSeconds()*6;
+    hour.style.transform=`rotate(${hr}deg)`;
+    min.style.transform=`rotate(${mn}deg)`;
+    sec.style.transform=`rotate(${sc}deg)`;
+    },1000);
